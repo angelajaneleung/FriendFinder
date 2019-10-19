90a8e0a21d374983ad6beeff67b38d1aff7b6284
Angela Jane Leung <angelajaneleung@gmail.com>
2019-10-19 13:51:36 -0400

    added friend list

---
+++ app/data/friend.js
@@ -0,0 +1,35 @@
+var friends = [
+	{
+		name: "Jake",
+		photo: "https://vignette.wikia.nocookie.net/brooklynnine-nine/images/7/7f/JakeS5.jpg/revision/latest?cb=20171103222710",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Jess",
+		photo: "https://vignette.wikia.nocookie.net/newgirl/images/2/29/Jess.jpg/revision/latest?cb=20120213201555&path-prefix=de",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Sweeny",
+		photo: "https://i.pinimg.com/originals/3c/76/5d/3c765d5deb85422067cf91cac0662c3d.jpg",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Sabrina",
+		photo: "https://fsmedia.imgix.net/b3/e9/bb/ba/369f/43cb/8a6d/bd93a5e7f05b/chilling-adventures-of-sabrina-ending.jpeg?auto=compress&h=1200&w=1200&crop=edges&fit=crop",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Michael",
+		photo: "https://imgix.bustle.com/uploads/image/2018/4/9/0e1587de-fd74-4e77-9264-62a0bf5e894a-nup_130296_0067.jpeg?w=1020&h=574&fit=crop&crop=faces&auto=format&q=70",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+    },
+    {
+        name: "Donna",
+        photo: "https://www.usanetwork.com/sites/usanetwork/files/styles/full_episodes_633x356/public/2018/07/suits_cast_donna_s8.jpg?itok=nHx9XiwS",
+        scores: [4, 2, 1, 4, 5, 4, 5, 3, 4, 5]
+    }
+    
+];
+
+module.exports = friends;
